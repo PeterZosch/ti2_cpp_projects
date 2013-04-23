ad46a0a45f101aa1bc0b74974e169b448cdd0838
Peter Zosch <tobias@theile.me>
2013-04-24 01:18:18 +0200

    Aufgabe 1 aufgeraeumt

--- Aufgabe1/src/main.cpp
+++ Aufgabe1/src/main.cpp
@@ -10,329 +10,269 @@ using namespace std;
 vector<string>::iterator it;
 
 //Funktionsprototypen
-int FileHandling( vector<string> &stringVec, char *fileName, int mode );
 
-int TableFunc( char *dName );
+int fileHandling(vector<string> &stringVec, char *fileName, int mode);
+int tableFunc(char *dname);
 
-int InlineEdit( vector<string> &stringVec, char *iFile, int mode,
-                 int newFile );
-
-void ChangeLine( vector<string> &stringVec, string newString,
-                unsigned int aktLine );
-
-int InsertLine( vector<string> &stringVec, vector<string>::iterator &it, 
-               string newString, unsigned int aktLine, int newFile );
-
-int DeleteLine( vector<string> &stringVec, vector<string>::iterator &it, 
-               unsigned int aktLine );
-
-int SearchString( vector<string> &stringVec, vector<string>::iterator &it, 
-                 string newString, unsigned int aktLine );
+int inlineEdit(vector<string> &stringVec, char *iFile, int mode, int newFile);
+void changeLine(vector<string> &stringVec, string newString, unsigned int aktLine);
+int insertLine(vector<string> &stringVec, vector<string>::iterator &it, string newString, unsigned int aktLine, int newFile);
+int deleteLine(vector<string> &stringVec, vector<string>::iterator &it, unsigned int aktLine);
+int searchString(vector<string> &stringVec, vector<string>::iterator &it, string newString, unsigned int aktLine);
 
 
-
-
-/*
-===============================================================================
-main
-
-Hauptmenü zum öffnen einer Datei, oder zum öffnen der Tabellenfunktion
-===============================================================================
-*/
 int main()
 {
-    char key;
-	int  mode = 0;          // 0=open, 1=save
+	char key;
+	int mode=0; // 0=open, 1=save
 	char fileName[42];
-	char dName[42];
-    fpurge( stdin );
-   	vector<string> stringVec;
+	char dname[42];
+
+    	vector<string> stringVec;
 	
-	do {
-        system( "clear" );
+	do{
+		system("clear");
 
 		cout << endl << "Hautpmenue";
 		cout << endl << "----------";
 		cout << endl <<	"(o) Datei oeffnen";
 		cout << endl << "(t) Tabellenfunktion";
 		cout << endl << "(q) Quit";
-		cout << endl << ( ">" );
 		
-        cin.get( key );
+		cout << endl << (">");
+	
+		cin.get(key);
 		cin.ignore();
 
-		switch ( key ) {
-			case 'o':	    mode = 0;
-					        cout << endl << "Dateinamen eingeben" << endl;
-    					    cout << "> ";
-        				    cin >> fileName;	
-					        FileHandling( stringVec, fileName, mode );
-					        break;
+		switch(key){
+			case 'o':	mode = 0;
+					cout << endl << "Dateinamen eingeben" << endl;
+    					cout << "> ";
+        				cin >> fileName;	
+					fileHandling(stringVec, fileName, mode);
+					break;
 			
 			case 't':      	cout << "\nDateinamen eingeben" << endl << "> "; 
-   					        cin >> dName;
-				        	TableFunc( dName );
-				        	break;
+   					cin >> dname;
+					tableFunc(dname);
+					break;
 
-			case 'q':	    return 0;
+			case 'q':	return 0;
+			
+			//default :	cout << "Falsche Eingabe";
+//					nanosleep(1000);
 		}
-
-	} while ( key != 'q' );
+	}while(key != 'q');
 
 	return 0;
 }
 
+/* Funktion um eine Datei zu öffnen und speichern, falls noch nicht vorhanden 
+   wird sie Erstellt.
 
-/*
-===============================================================================
-FileHandling
+   Datei existiert und besitzt Inhalt:
+	Zeilen werden gezählt und als pointer zur Verfügung gestellt
+	Der inhalt wird in den Hauptspeicher gelesen vi der Vector-Klasse
+        und die Datei geschlossen
 
-Öffnen einer Datei, zeilenweise einlesen in Vektoren, und schließen der Datei
-gesamte Datei ist dadurch im Hauptspeicher und kann beliebig manipuliert
-werden
-Speichern: erneutes öffnen der Datei und gesamten Inhalt im Hauptspeicher
-           in Datei schreiben.
-===============================================================================
-*/
-int FileHandling( vector<string> &stringVec, char *fileName, int mode )
+   Datei existiert nicht:
+	Vector-Klasse wird erstellt aber ohne Inhalt */
+
+int fileHandling(vector<string> &stringVec, char *fileName, int mode)
 {
-	string null = ""; 
-   	string buffer;
-   	char   choice;
-   	string enter;
-   	int    newFile = 0;
+    string null=""; 
+    string buffer;
+    char choice;
+    string enter;
+    int newFile = 0;
  
-   	ifstream iFile;
-   	ofstream oFile;
+    ifstream iFile;
+    ofstream oFile;
     
-    if ( mode == 0 ) {                  //Öffnen
-	    stringVec.clear(); 
-       	iFile.open( fileName );
+    if(mode == 0){                  //Öffnen
         
-        if ( !iFile ) {
+        iFile.open(fileName);
+        
+        if(!iFile){
             cout << endl << "Datei existiert nicht !" << endl;
-           	cout << " Diese Datei erzeugen? (y/n)" << endl << "> ";
-           	cin.ignore();
-	    	cin.get( choice );            
+            cout << " Diese Datei erzeugen? (y/n)" << endl << "> ";
+	   
+	    cin.get(choice);            
+            cin.ignore();
             
-           	if ( choice == 'y' ) {
-				stringVec.push_back( null );
-	       		newFile = 1;
-				iFile.close();
-	       		InlineEdit( stringVec, fileName, mode, newFile );
-				return 0;
+            if (choice == 'y'){
+		stringVec.push_back(null);
+	        newFile = 1;
+		iFile.close();
+        	inlineEdit(stringVec, fileName, mode, newFile);
 	
-	    	} else if ( choice == 'n' ) {
-               	return 0;
-           	}
+	    }else if(choice == 'n'){
+                return 0;
+            }
         }
 
-        while ( getline( iFile, buffer ) ) {
-           	stringVec.push_back( buffer );
-       	}
-       	iFile.close();
-       	InlineEdit( stringVec, fileName, mode, newFile );
-
-    } else if ( mode == 1 ) {            //Speichern
-        oFile.open( fileName );
-
-        for ( it = stringVec.begin() ; it != stringVec.end() ; ++it ) {
-        		oFile << *it << endl;
-       	}	
-
-       	oFile.close();
-
-		it = stringVec.begin(); 
-       	
+        
+        while(getline(iFile, buffer)){
+            
+            stringVec.push_back(buffer);
+        
+        }
+        
+        iFile.close();
+        cin.ignore(); 
+        inlineEdit(stringVec, fileName, mode, newFile);
+        
+    }else if(mode == 1){            //Speichern
+    
+        oFile.open(fileName);
+    
+        for (it = stringVec.begin(); it != stringVec.end(); ++it){
+        
+            oFile << *it << endl;
+        }
+        
+        oFile.close();
+       
+	it = stringVec.begin(); 
         system("clear");
-		cout << "Datei " << "'" << fileName << "'" << " geschrieben" << endl;
+	cout << "Datei " << "'" << fileName << "'" << " geschrieben" << endl;
+        
     	cout << "\n\n\nplease press Return to continue" << endl;
-   		cin.ignore(); 
-		getline( cin, enter );
-       	
-    	InlineEdit( stringVec, fileName, mode, newFile );
-   	}
-   	return 0;
+   	cin.ignore(); 
+	getline(cin, enter);
+
+    }
+        inlineEdit(stringVec, fileName, mode, newFile);
+    //return 0;
 }
 
-
-/*
-===============================================================================
-InlineEdit
-
-Hauptfunktion zum inline Editieren der eingelesenen Textdatei
-Funktionen:
-    vorwärts und rückwärts Blättern
-    aktuelle Zeile ändern
-    neue Zeile einfügen
-    aktuelle Zeile löschen
-    Datei nach Suchbegriff durchsuchen
-    Datei speichern
-    zurück in das Hauptmenü
-===============================================================================
-*/
-int InlineEdit( vector<string> &stringVec, char *fileName, int mode, 
-                int newFile )
+int inlineEdit(vector<string> &stringVec, char *fileName, int mode, int newFile)
 {   
-	char         key;
+	char key;
 	unsigned int aktLine = 1;
-	string       newString;
-	string       anyKey;
+	string newString;
+	string anyKey;
 
 	it = stringVec.begin();
 
-	do {
-		system( "clear" );
-		
-        cout << "(r)ück, (v)or | (a)endern, (e)infuegen, (l)oeschen, "
-                "s(u)chen | (s)ave | (b)ack\n";
-		cout << "----------------------------------------------------"
-                "--------------------------";
+	do{
+		system("clear");
+		cout << "(r)ück, (v)or | (a)endern, (e)infuegen, (l)oeschen, s(u)chen | (s)ave | (b)ack\n";
+		cout << "------------------------------------------------------------------------------";
 		cout << "\n";
-        
-        cout << " " << *it << "\n";      //Zeilen ausgabe über Iterator
+
+                	cout << " " << *it << "\n";
 		
-		cout << "-----------------------------------------------------"
-                "-------------------(" << aktLine << "/" 
-             << stringVec.size() << ")";
+		cout << "------------------------------------------------------------------------(" << aktLine << "/" << stringVec.size() << ")";
+        
 		cout << "\n>";
            
-		cin.get( key );
+		cin.get(key);
 		cin.ignore();
 		
-		switch ( key ) {
-			case 'v':       aktLine++;
-					        if ( it != (stringVec.end() -1 ) ) {
-					        	++it;
-				        	} else {
-					        	it = stringVec.end() -1;
-						        aktLine = stringVec.size();
-					        }
-                    		break;
+		switch(key){
+			case 'v':	aktLine++;
+					if(it != (stringVec.end() -1)){
+						++it;
+					}else{
+						it = stringVec.end() -1;
+						aktLine = stringVec.size();
+					}
+                    			break;
                		
-			case 'r':   	aktLine--;
-					        if ( it == stringVec.begin() ) {
-						        it = stringVec.begin();
-						        aktLine=1;
-					        } else {
-						        --it;
-					        }
-                    		break;
+			case 'r':	aktLine--;
+					if(it == stringVec.begin()){
+						it = stringVec.begin();
+						aktLine=1;
+					}else{
+						--it;
+					}
+                    			break;
               		
-			case 'a':       cout << "\nAktuelle Zeile ändern\n>";
-                            getline( cin, newString );
-			        		ChangeLine( stringVec, newString, aktLine );
-				        	break;
+			case 'a':
+					cout << "\nAktuelle Zeile ändern\n>";
+                                        getline(cin, newString);
+					changeLine(stringVec, newString, aktLine);
+					break;
 
-			case 'e':	    cout << "\nNeue Zeile einfügen\n>";
-				        	getline( cin, newString );
-			   	aktLine =   InsertLine( stringVec, it, newString, aktLine,
-                                        newFile );
-				        	break;	
+			case 'e':	cout << "\nNeue Zeile einfügen\n>";
+					getline(cin, newString);
+					aktLine = insertLine(stringVec, it, newString, aktLine, newFile);
+					break;	
 
-			case 'l':	    aktLine = DeleteLine( stringVec, it, aktLine );
-				        	break;
+			case 'l':	aktLine = deleteLine(stringVec, it, aktLine);
+					
+					break;
 		
-			case 'u':   	cout << "\nSuchstring eingeben\n>";
-                            getline( cin, newString );
-				aktLine =   SearchString( stringVec, it, newString, aktLine );
-				        	if ( aktLine == 4711 ) {
-					        	cout << "\nSuchstring nicht gefunden - "
-                                        "Press Return to continue\n";
-					    	    getline( cin, anyKey );
-						        aktLine = 1;
-					        }
-					        break;												
+			case 'u':	cout << "\nSuchstring eingeben\n>";
+                                        getline(cin, newString);
+					aktLine = searchString(stringVec, it, newString, aktLine);
+					if (aktLine == 4711){
+						cout << "\nSuchstring nicht gefunden - Press Return to continue\n";
+						getline(cin, anyKey);
+						aktLine = 1;
+					}
+					break;												
         
-			case 's':	    mode = 1;	 //speichermodus aktivieren
-					        FileHandling( stringVec, fileName, mode );
-					        return 0;												
+			case 's':	mode = 1;	 //speichermodus aktivieren
+					fileHandling(stringVec, fileName, mode);
+					return 0;												
 
-	      	case 'b':	    return 0;
+		      	case 'b':	return 0;
+	
+			//default:	cout << "Falsche Eingabe";
 		}
-	} while ( key != 'q' );
+	}while(key != 'q');
+	
 	return 0;
 }
 
 
-/*
-===============================================================================
-InsertLine
 
-Fügt nach der angezeigten Zeile eine neue Zeile ein
-===============================================================================
-*/
-int InsertLine( vector<string> &stringVec, vector<string>::iterator &it, 
-                string newString, unsigned int aktLine, int newFile )
+int insertLine(vector<string> &stringVec, vector<string>::iterator &it, string newString, unsigned int aktLine, int newFile)
 {
-	if ( newFile == 1 ) {
-		stringVec.push_back( newString );
+	if(newFile == 1){
+		stringVec.push_back(newString);
 		aktLine++;
 		return aktLine;
 	}
 	++it;		//damit nach der aktuellen Zeile der newString eingefügt wird	
-	stringVec.insert( it, newString );
+	stringVec.insert(it, newString);
 	aktLine++;
 	return aktLine;	
 }
 
 
-/*
-===============================================================================
-DeleteLine
-
-löscht die aktuell angezeigte Zeile
-===============================================================================
-*/
-int DeleteLine( vector<string> &stringVec, vector<string>::iterator &it,
-                 unsigned int aktLine )
+int deleteLine(vector<string> &stringVec, vector<string>::iterator &it, unsigned int aktLine)
 {
-	stringVec.erase( it );
-	if ( aktLine == stringVec.size() ) {
+	stringVec.erase(it);
+	if(aktLine == stringVec.size()){
 		aktLine--;
 		--it;
-		it = stringVec.end() - 1;
+		it = stringVec.end()-1;
 	}
+
 	return aktLine;
 }
 
 
-/*
-===============================================================================
-ChangeLine
-
-Die aktuell angezeigte Zeile kann geändert werden (wird komplett ersetzt)
-===============================================================================
-*/
-void ChangeLine( vector<string> &stringVec, string newString,
-                 unsigned int aktLine )
+void changeLine(vector<string> &stringVec, string newString, unsigned int aktLine)
 {
-	stringVec[aktLine - 1] = newString;
+	stringVec[aktLine-1] = newString;
 }
 
 
-/*
-===============================================================================
-SearchString
-
-Durchsucht die Datei nach einem Suchbegriff.
-    gefunden:       die Zeile in welcher der Suchbegriff gefunden wurde wird 
-                    angezeigt
-    nicht gefunden: eine Meldung wird ausgegeben
-===============================================================================
-*/
-int SearchString( vector<string> &stringVec, vector<string>::iterator &it, 
-                  string newString, unsigned int aktLine )
+int searchString(vector<string> &stringVec, vector<string>::iterator &it, string newString, unsigned int aktLine)
 {
-	system( "clear" );
+	system("clear");
 	aktLine = 1;
 	string buffer;
 
-	for ( it = stringVec.begin() ; it != stringVec.end() ; ++it) {
+	for (it = stringVec.begin(); it != stringVec.end(); ++it){
 		buffer = *it;
-		if ( buffer.find( newString ) != buffer.npos ) {
+		if(buffer.find(newString) != -1){
 			return aktLine;
-		} else {
+		}else{
 			aktLine++;
 		}
 	}
@@ -341,63 +281,49 @@ int SearchString( vector<string> &stringVec, vector<string>::iterator &it,
 	return aktLine;
 }
 
-
-/*
-===============================================================================
-TableFunc
-
-Funktion zum einlesen einer Datei mit Zahlenwerten
-    Berechnungen:
-        Summe aller Zahlen
-        Mittelwert der Zahlen
-        Varianz
-===============================================================================
-*/
-int TableFunc( char *dName )
+int tableFunc(char *dname)
 {
-	system( "clear" ); 
-	double amt, mean, vrne, zahl, temp, sum;
-           amt = mean = vrne = zahl = temp = sum = 0;	 
+    system("clear"); 
+    float amt, mean, vrne, zahl, temp, sum;
+    amt = mean = vrne = zahl = temp = sum = 0;	 
     string buffer, zosh;
     
-   	ifstream tfile;
-   	tfile.open(dName);
     
-   	vector<int> numList;
-    vector<int>::iterator it_1;
- 
-   	while ( getline( tfile, buffer ) ) {
-       	zahl = atoi( buffer.c_str() );
-       	numList.push_back( zahl );
-   	}
+    ifstream tfile;
+    tfile.open(dname);
     
-   	amt = numList.size();
-   	cout << "Zahlen der Datei:\n";
+    vector<int> numList;
+    
+    while(getline(tfile, buffer)){
+        
+        zahl = atoi(buffer.c_str());
+        numList.push_back(zahl);
+    }
+    
+    amt = numList.size();
+    cout << "Zahlen der Datei:\n";
 	 
-   	for ( it_1 = numList.begin() ; it_1 != numList.end() ; ++it_1 ) {
-       	cout << *it_1 << ", ";
-       	sum += *it_1;
-   	}
-   
-	cout << "\n";
-   	cout << "\n\n\nSumme ist : " << sum;
+    for (vector<int>::iterator it1 = numList.begin(); it1 != numList.end(); ++it1){
+        cout << *it1 << ", ";
+        sum += *it1;
+    }
+    cout << "\n";
+    cout << "\n\n\nSumme ist : " << sum;
     
-   	amt = numList.size();
-   	mean = sum / amt;
-   	cout << "\n\nMittelwert ist : " << mean;
+    amt = numList.size();
+    mean = sum / amt;
+    cout << "\n\nMittelwert ist : " << mean;
     
-   	for ( it_1 = numList.begin() ; it_1 != numList.end() ; ++it_1 ) {
-        	temp += ( *it_1 - mean ) * ( *it_1 - mean );
-   	}
+    for (vector<int>::iterator it1 = numList.begin(); it1 != numList.end(); ++it1){
+        temp += pow((*it1 - mean), 2) ;
+    }
     
-   	vrne = temp / amt;
-   	cout << "\n\nVarianz ist : " << vrne << endl;
+    vrne = temp / amt;
+    cout << "\n\nVarianz ist : " << vrne << endl;
     
-   	cout << "\n\n\nplease press Return to continue" << endl;
-   	cin.ignore(); 
-   	getline(cin, zosh);
+    cout << "\n\n\nplease press Return to continue" << endl;
+    cin.ignore(); 
+    getline(cin, zosh);
     
-   	return 0; 
+    return 0; 
 }
-	
-// vim: et sw=4 ts=4
