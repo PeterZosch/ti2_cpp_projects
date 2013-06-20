39af265941559fa359804796610b7e200847c34b
Alex <alexander.koch@atomtown.de>
2013-06-20 12:51:10 +0200

    myWidget in rlcwidget umbenannt

---
+++ Aufgabe4/rlcwidget.h
@@ -0,0 +1,24 @@
+#include <QtGui>
+#include <QWidget>
+ 
+class RLCWidget : public QWidget
+{
+    Q_OBJECT
+	
+signals:
+    void mouseClickEvent();
+
+public:
+
+protected:
+	void mouseReleaseEvent ( QMouseEvent * e );
+	void mousePressEvent ( QMouseEvent * e );
+	 
+private:
+	QPolygonF rlccurve;
+    void paintEvent(QPaintEvent *event);
+	void compute(); 
+	QPoint m_lastPoint;
+	bool m_mouseClick;
+	int click;
+};
