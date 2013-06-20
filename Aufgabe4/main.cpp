fe59b4638d83b1de37793b8f7d5f6060d4ecdf68
Peter Zosch <tobias@theile.me>
2013-06-20 12:24:46 +0200

    funktionierende version von RLC-sim QT

--- Aufgabe4/main.cpp
+++ Aufgabe4/main.cpp
@@ -1,53 +1,15 @@
-#include <QtGui>  
-#include </usr/include/qwt/qwt_plot.h>
-#include </usr/include/qwt/qwt_plot_curve.h>
-
-
-class CosinusData: public QwtSyntheticPointData
-{
-public:
-    CosinusData(size_t numberOfPoints):
-        QwtSyntheticPointData(numberOfPoints)
-    {
-    }
-    
-    //This has to be overridden, because it is pure virtual in base class
-    double y(double x) const
-    {
-        return qCos(x);
-    }
-};
-
-
-int main(int argc, char **argv)
-{
-  QApplication app(argc, argv);
-  
-    QwtPlot *myPlot = new QwtPlot(NULL);   
-    
-    // add curves
-    QwtPlotCurve *curve1 = new QwtPlotCurve("Curve 1");
-    QwtPlotCurve *curve2 = new QwtPlotCurve("Curve 2");
-    
-    myPlot->setFixedWidth(300);
-    myPlot->setWindowTitle("Cosinus and Straight Line Graph");
-    
-    
-    double x[3] ={0,10,15};
-    double y[3] ={0,0,0};
-    curve1->setData(new QwtCPointerData(&x[0],&y[0],(size_t)3));    
-    
-    //Create QwtSyntheticPointData (Cosinus data) with 1000 points
-    //You can increase number of points to generate smoother graphs
-    curve2->setData(new CosinusData(1000));
+#include <QtGui>
+#include "mywidget.h"
  
-    
-    curve1->attach(myPlot);
-    curve2->attach(myPlot);
-   
-    // finally, refresh the plot
-    myPlot->replot();
-    myPlot->show();  
-    
+int main( int argc, char **argv )
+{
+	QApplication app( argc, argv );
+ 
+	MyWidget myWidget;
+	
+	myWidget.resize(1200, 600);
+	myWidget.setWindowTitle("RLC-Simulator by TaK");
+ 	myWidget.show();
+ 
   return app.exec();
 }
